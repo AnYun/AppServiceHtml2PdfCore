e31bcc74598fad9a593cba0a24ed21e57d19ed18
AnYun <coding@anyun.idv.tw>
2021-05-02 15:32:53 +0800

    Add Wkhtmltopdf.NetCore

--- AppServiceHtml2PdfCore/Startup.cs
+++ AppServiceHtml2PdfCore/Startup.cs
@@ -8,6 +8,7 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Wkhtmltopdf.NetCore;
 
 namespace AppServiceHtml2PdfCore
 {
@@ -24,6 +25,7 @@ namespace AppServiceHtml2PdfCore
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddControllersWithViews();
+            services.AddWkhtmltopdf();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
